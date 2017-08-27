ff63b1893fe40c3e06e1d2ca5d32155c3058934c
Tore Brede <tore.brede@gmail.com>
2017-08-27 17:19:15 +0200

    Initial commit

---
+++ ontology/src/main/java/com/kjipo/ontology/JenaTest.java
@@ -0,0 +1,47 @@
+package com.kjipo.ontology;
+
+
+import com.google.common.collect.Lists;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+
+public class JenaTest {
+
+
+
+    private void createOntology() {
+        Model model = ModelFactory.createDefaultModel();
+
+        Resource resource = model.createResource();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    public static void main(String args[]) {
+        JenaTest jenaTest = new JenaTest();
+
+        jenaTest.createOntology();
+
+
+
+
+    }
+
+
+
+
+
+}
