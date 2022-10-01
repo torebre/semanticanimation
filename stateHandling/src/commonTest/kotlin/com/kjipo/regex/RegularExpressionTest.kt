e2b43da742811a4ffe6b917606c765ed771213e3
Tore Brede <tore.brede@gmail.com>
2022-10-01 09:58:19 +0200

    Updating test

--- stateHandling/src/commonTest/kotlin/com/kjipo/regex/RegularExpressionTest.kt
+++ stateHandling/src/commonTest/kotlin/com/kjipo/regex/RegularExpressionTest.kt
@@ -2,29 +2,27 @@ package com.kjipo.regex
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class RegularExpressionTest {
 
     @Test
-    fun testRegularExpression() {
-        val regularExpression = RegularExpression()
-        val postfixNotation = regularExpression.regularExpressionToPostfixNotation("a(b|c)*d")
-
-        assertEquals("abc|*.d.", postfixNotation)
+    fun testPostfixNotationConversion() {
+        assertEquals("abc|*.d.", RegularExpression.regularExpressionToPostfixNotation("a(b|c)*d"))
+        assertEquals("a+", RegularExpression.regularExpressionToPostfixNotation("a+"))
     }
 
 
     @Test
     fun testRegularExpression2() {
-        val regularExpression = RegularExpression()
-//        val result = regularExpression.doMatch("a*aa", "aaabbaaa")
-        val result = regularExpression.doMatch("a+", "a")
+        assertTrue { doRegExpMatch("a+", "a") }
 
-        println("Result: ${result}")
-
-        assertTrue { result }
+        assertFalse { doRegExpMatch("a+", "b") }
     }
+
+    private fun doRegExpMatch(regularExpression: String, inputString: String) =
+        RegularExpression("a+", "a").doMatch()
 
 
 }
